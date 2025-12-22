aabf826967b338a75bc0e2ad9afbb2ab99cb4e70
AmnaHussien <amnahussin88@gmail.com>
2025-12-22 19:19:55 +0200

    final edition

--- Face_Mask_Detection_backup/train_mask_detector.py
+++ Face_Mask_Detection_backup/train_mask_detector.py
@@ -16,7 +16,6 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from imutils import paths
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 
